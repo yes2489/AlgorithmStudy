a919d1146436a9c5fb2289055626a7849b4c35a2
양은서 <dmstj4871@naver.com>
2025-07-19 19:51:30 +0900

    [BOJ] 2798 / 블랙잭 / 브론즈2 / eunseoyang

---
+++ EunseoYang/week02/BOJ2798.py
@@ -0,0 +1,16 @@
+# N장의 카드를 바닥에 놓음
+# 외친 M을 넘지 않으면서 M과 최대한 가깝게 (3장)
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+card = list(map(int, input().split()))
+
+# 3장의 합이 M을 넘지 않으면서 M과 최대한 가까우면 출력
+max_total=0
+for i in range(len(card)):
+    for j in range(i+1, len(card)):
+        for k in range(j+1, len(card)):
+            total = card[i]+card[j]+card[k];
+            if total <= M:
+                max_total = max(max_total, total)
+print(max_total)
