2519e0146e499700b6e73ee9b23b00c3aee218bd
Jay Youngjae <jea71882@naver.com>
2025-08-07 11:55:30 +0900

    [BOJ] 2606 바이러스 / 실버3 / Jay-YoungJae

---
+++ YoungjaeKim/week4/BOJ2606.py
@@ -0,0 +1,30 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+n = int(input()) #컴퓨터의 수
+m = int(input()) # 쌍의 수
+start = 1
+graph = [[] for _ in range(n+1)]
+edges = [list(map(int, input().split())) for _ in range(m)]
+
+for u, v in edges:
+    graph[u].append(v)
+    graph[v].append(u)
+
+
+visited = [False] * (n+1) # 방문한 컴퓨터 체크
+
+def bfs(start, visited):
+    queue = deque([start])
+    visited[start] = True
+    result = 0 # 감염된 컴퓨터 수
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i]=True
+                result +=1
+    return result
+
+print(bfs(1, visited))
