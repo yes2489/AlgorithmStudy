ed7f10469f60adc2d87129d010406ed027095b57
Jay Youngjae <jea71882@naver.com>
2025-08-26 17:56:23 +0900

    [BOJ] 2839 설탕 배달 / 실버4 / Jay-YoungJae

---
+++ YoungjaeKim/week6/BOJ2839.py
@@ -0,0 +1,18 @@
+def sugar(N, idx, x, y):
+    min_sugar_cnt = 9999
+    for i in range(N):
+        for j in range(N):
+            if (i * x) + (j * y) == N:
+                min_sugar_cnt = min(i+j, min_sugar_cnt)
+
+    if min_sugar_cnt == 9999:
+        return -1
+    else:
+        return min_sugar_cnt
+
+
+
+
+N = int(input())
+print(sugar(N, 0, 3, 5))
+#생각해보니 그냥 3이랑 5로 나누면 되겟다
